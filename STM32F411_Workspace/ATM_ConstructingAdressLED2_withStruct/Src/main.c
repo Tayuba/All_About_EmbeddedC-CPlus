55f97d4f0271466dde6847d0936624e0d099c114
Tayuba <ayubamonnietahiru@gmail.com>
2023-03-18 21:27:40 +0100

    LED Toggle Using St header files

--- STM32F411_Workspace/ATM_ConstructingAdressLED2_withStruct/Src/main.c
+++ STM32F411_Workspace/ATM_ConstructingAdressLED2_withStruct/Src/main.c
@@ -3,22 +3,22 @@
 #define Periph_Base 						(0x40000000UL)
 
 //AHB1 base address
-#define AHB1_Periph_Offset			(0x20000L)
+#define AHB1_Periph_Offset					(0x20000L)
 #define AHB1_Base							(Periph_Base + AHB1_Periph_Offset)
 
 //LED2 base address
 #define GPIOA_Offset						(0x0UL)
-#define GPIOA_Base						(AHB1_Base + GPIOA_Offset)
+#define GPIOA_Base							(AHB1_Base + GPIOA_Offset)
 
 //Reset and clock control(RCC)
-#define RCC_Offset 						(0x3800UL)
+#define RCC_Offset 					    	(0x3800UL)
 #define RCC_Base							(AHB1_Base + RCC_Offset)
 
 //Set port A to 1
-#define GPIOA_EN							(1U<<0)
+#define GPIOA_EN						    (1U<<0)
 
 //Setting Pin 5 to 1
-#define Pin5										(1UL<<5)
+#define Pin5								(1UL<<5)
 #define LED_PIN								Pin5
 
 typedef struct
@@ -35,7 +35,7 @@ typedef struct
 	volatile uint32_t ODR;
 }GPIO_TypeDef;
 
-#define RCC 									((RCC_TypeDef*)RCC_Base)
+#define RCC 								((RCC_TypeDef*)RCC_Base)
 #define GPIOA 								((GPIO_TypeDef*)GPIOA_Base)
 int main(void)
 {
