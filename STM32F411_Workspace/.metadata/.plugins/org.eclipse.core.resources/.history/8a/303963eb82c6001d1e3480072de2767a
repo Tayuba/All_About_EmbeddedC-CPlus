ad4dda24a18ed97c0339e7d647dece851f3fa124
Tayuba <ayubamonnietahiru@gmail.com>
2023-03-19 19:58:08 +0100

    UART Rx Driver

---
+++ STM32F411_Workspace/.metadata/.plugins/org.eclipse.core.resources/.history/8a/303963eb82c6001d1e3480072de2767a
@@ -0,0 +1,19 @@
+/*
+ * UART.h
+ *
+ *  Created on: Mar 19, 2023
+ *      Author: ayuba
+ */
+#include <stdint.h>
+
+#ifndef UART_H_
+#define UART_H_
+
+
+
+
+#endif /* UART_H_ */
+#include "stm32f4xx.h"
+
+
+void UART2_Tx_Init(void);
---
+++ STM32F411_Workspace/ATM_UART_Rx/Debug/Src/main.d
@@ -0,0 +1,19 @@
+Src/main.o: ../Src/main.c \
+ D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Include/core_cm4.h \
+ D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Include/cmsis_version.h \
+ D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Include/cmsis_compiler.h \
+ D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Include/cmsis_gcc.h \
+ D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Include/mpu_armv7.h \
+ D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Src/UART.h
+D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Include/core_cm4.h:
+D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Include/cmsis_version.h:
+D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Include/cmsis_compiler.h:
+D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Include/cmsis_gcc.h:
+D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Include/mpu_armv7.h:
+D:/All_About_EmbeddedC-C++/STM32F411_Workspace/Chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Src/UART.h:
